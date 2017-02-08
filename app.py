c7b2fd2cfdc27d7d1c2624badc29e719e8cd3cf7
Valentine Ganansia <valentineganansia@gmail.com>
2017-02-08 13:38:46 +0200

    DEMO

--- app.py
+++ app.py
@@ -29,5 +29,105 @@ def index():
         ret += '%s=%s\n' % (k, v)
 
     return ret
+from bottle import route, run, template, static_file, request
+import random
+import json
+import pymysql
+
+connection= pymysql.connect(host="localhost",
+                             user="root",
+                             password="",
+                             db="adventure",
+                             charset="utf8",
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+@route("/", method="GET")
+def index():
+    return template("adventure.html")
+
+
+@route("/start", method="POST")
+def start():
+    username = request.POST.get("name")
+    current_adv_id = request.POST.get("adventure_id")
+    print(username)
+    # current_story_id=0
+
+# def gameOver(life,current_step):
+#     try:
+#     with connection.cursor() as cursor:
+#         end_game = True
+#     print(end_game)
+
+
+    user_id = 0 #todo check if exists and if not create it
+    current_story_id = 0 #todo change
+    next_steps_results = [
+        {"id": 1, "option_text": "I fight it"},
+        {"id": 2, "option_text": "I give him 10 coins"},
+        {"id": 3, "option_text": "I tell it that I just want to go home"},
+        {"id": 4, "option_text": "I run away quickly"}
+        ]
+
+
+
+    #todo add the next step based on db
+    return json.dumps({"user": user_id,
+                       "adventure": current_adv_id,
+                       "current": current_story_id,
+                       "text": "You meet a mysterious creature in the woods, what do you do?",
+                       "image": "creature.jpg",
+                       "options": next_steps_results
+                       })
+
+
+@route("/story", method="POST")
+def story():
+    user_id = request.POST.get("user")
+    current_adv_id = request.POST.get("adventure")
+    next_story_id = request.POST.get("next") #this is what the user chose - use it!
+    next_steps_results = [
+        {"id": 1, "option_text": "I run!"},
+        {"id": 2, "option_text": "I hide!"},
+        {"id": 3, "option_text": "I sleep!"},
+        {"id": 4, "option_text": "I fight!"}
+        ]
+    random.shuffle(next_steps_results) #todo change - used only for demonstration purpouses
+
+    #todo add the next step based on db
+    return json.dumps({"user": user_id,
+                       "adventure": current_adv_id,
+                       "text": "New scenario! What would you do?",
+                       "image": "choice.jpg",
+                       "options": next_steps_results
+                       })
+# def insertUser(username):
+#     with connection.cursor() as cursor:
+#         sql = "INSERT INTO users(`username`) VALUES (%s)"
+#         cursor.execute(sql, username)
+#         connection.commit()
+
+@route('/js/<filename:re:.*\.js$>', method='GET')
+def javascripts(filename):
+    return static_file(filename, root='js')
+
+
+@route('/css/<filename:re:.*\.css>', method='GET')
+def stylesheets(filename):
+    return static_file(filename, root='css')
+
+
+@route('/images/<filename:re:.*\.(jpg|png|gif|ico)>', method='GET')
+def images(filename):
+    return static_file(filename, root='images')
+
+def main():
+    run(host='localhost', port=9001)
+
+if __name__ == '__main__':
+    main()
+
+
 
 bottle.run(host='0.0.0.0', port=argv[1])
